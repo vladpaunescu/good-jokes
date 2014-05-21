ce9e6fe877017fb2163f351b71dd7abdfb970042
Vlad Paunescu <vlad.paunescu@cti.pub.ro>
2014-05-22 00:18:21 +0300

    Cleanup

--- jokes-analyzer/src/main/java/ro/pub/cti/jokes/analyzer/WordCountStatistics.java
+++ jokes-analyzer/src/main/java/ro/pub/cti/jokes/analyzer/WordCountStatistics.java
@@ -1,10 +1,6 @@
 package ro.pub.cti.jokes.analyzer;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
